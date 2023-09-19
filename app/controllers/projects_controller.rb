f81ae88be0829f6e844473ce1d50dd247ebdcecf
deniszackharov <denis.zaharov@flatstack.com>
2023-09-19 19:32:18 +0300

    feature: add crud for projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,48 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update destroy]
+
+  def index
+    @projects = Project.all.order(:id)
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path, notice: "Update Successful"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path, notice: "Created Successful"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path, notice: "Project destroyed"
+  end
+
+  private
+
+  def set_project
+    @project = Project.find_by(id: params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+end
