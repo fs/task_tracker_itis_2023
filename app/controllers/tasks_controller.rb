872de611a53007501d302c7565e7e1791b8bf49e
elenagernichenko <elena.gernichenko@yandex.ru>
2023-09-23 19:09:23 +0300

    added functionality for viewing all project tasks, specific project tasks, creating tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  before_action :set_project
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = @project.tasks.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = @project.tasks.build
+  end
+
+  def create
+    @task = @project.tasks.build(task_params)
+
+    if @task.save
+      redirect_to project_tasks_path(@project), notice: "Task created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to project_tasks_path(@project), notice: "Task updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to project_tasks_path(@project), notice: "Task destroyed"
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_task
+    @task = @project.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :status, :deadline)
+  end
+end
