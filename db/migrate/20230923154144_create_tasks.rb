c9a985b1d4ff878dc0276116472170b616a58df9
Stepan Tischenko <stepantishhen@mail.ru>
2023-09-23 20:49:38 +0300

    Update db/migrate/20230923154144_create_tasks.rb
    Co-authored-by: Denis Zakharov <78980031+DenisZackharov@users.noreply.github.com>

--- db/migrate/20230923154144_create_tasks.rb
+++ db/migrate/20230923154144_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
     create_table :tasks do |t|
       t.string :title
       t.text :description
-      t.boolean :state, default=false
+      t.boolean :state, default: false
       t.datetime :deadline
       t.references :project, null: false, foreign_key: true
 
