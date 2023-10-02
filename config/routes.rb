92f86ced50b67b393e7c288b72df47518ad097f4
4delych <adel.bagmanov@mail.ru>
2023-10-02 03:01:58 +0300

    hw3_part1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/create'
   root "projects#index"
 
-  resources :projects
+  resources :projects do
+    resources :tasks, only: [:index, :create, :new, :show]
+  end
 end
