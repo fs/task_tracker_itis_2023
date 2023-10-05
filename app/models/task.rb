658b0db7e37ac0b140be6c94f1b9c3788a6d45e9
root <tammam744@gmail.com>
2023-10-05 12:32:15 +0300

    .

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
 
+  #enum :state {Just started:0, In Progress:1, Finished:2}
   belongs_to :project
 
   validates :task_title, :task_description, :state, :deadline, :project_id, presence: true
