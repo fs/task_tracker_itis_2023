8e9f86da07a301e2e226f9d414640879339234ec
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-10-10 02:43:00 +0300

    add template for session realization

---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,2 @@
+module Authentication
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
+  def show
+  end
+
   def new
   end
 
-  def show
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
