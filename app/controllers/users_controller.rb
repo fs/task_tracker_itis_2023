15a3d5ef22954b97c030468cced37d763fa5e6d1
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-10-10 01:32:53 +0300

    set flashes and refactor views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, notice: "You've successfully signed up!"
     else
       render :new
     end
