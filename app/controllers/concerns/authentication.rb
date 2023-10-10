6f7eb53c9178a317912496c90fa33e6ec8125401
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-10-10 03:02:29 +0300

    first step of session realisation

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,2 +1,15 @@
 module Authentication
+  extend ActiveSupport::Concern
+
+  class UserNotAuthenticated < StandardError; end
+
+  included do
+    rescue_from UserNotAuthenticated, with: :not_authenticated!
+
+    helper_method :current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:current_user_id])
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(email: user_params[:email])
+            &.authenticate(user_params[:password])
+
+    if @user
+      session[:current_user_id] = @user.id
+      redirect_to root_path, notice: "You've successfully logged in!"
+    else
+      @user = User.new
+      @user.errors.add :base, "Wrong email or password"
+      render :new
+    end
   end
 
   private
