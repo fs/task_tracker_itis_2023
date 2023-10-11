60a32a4b613d204ac4c6e0ab2ef6b1eeb92757f0
elenagernichenko <elena.gernichenko@yandex.ru>
2023-10-11 18:23:35 +0300

    added password & email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
+
+  validates :password, length: { minimum: 6 }, on: :create
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "has an invalid format" }
 end
