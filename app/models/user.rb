33f6c6d25f7f6e391e6efae7deddeb021efce5e3
root <tammam744@gmail.com>
2023-10-11 23:26:35 +0300

    User Validations & Profile
    Added password length validation, and email format "Someone@Example.com" validation
    Added the profile view to the navbar when the user is signed in!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be in the format someone@example.com" }
   validates :first_name, :last_name, presence: true
+  validates :password, length: {minimum: 6, allow_nil: false}
 end
