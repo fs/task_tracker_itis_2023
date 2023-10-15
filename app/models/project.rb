ec0504af2e338f302f3605b2d9211da805999b2e
maryamk26 <maryamkareem68@gmail.com>
2023-10-15 22:02:02 +0300

    Homework 5

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
-  validates :name, :description, presence: true
+  validates :name, :description, presence: true, uniqueness: true
   has_many :tasks, dependent: :destroy
 end
---
+++ db/migrate/20231015184702_unique_task_name.rb
@@ -0,0 +1,5 @@
+class UniqueTaskName < ActiveRecord::Migration[6.1]
+  def change
+    add_index :tasks, %i[name project_id], unique: true
+  end
+end
