707e5c85a39e46a27ad762d8284ee8a50bda4d74
Abdoelmakarem Youssef Magdy Anwar Abdelfattah <youssefmagdyanwar@yandex.ru>
2023-10-16 19:31:22 +0300

    added rail-level Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Invalid email format" }
+  validates :password, presence: true, length: { minimum: 6 }
 end
