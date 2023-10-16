f4cd5c6ac0dd28880b076075deaaa37ac3f99577
Dariya Zhukova <dariyazhukova@MacBook-Air-Dariya.local>
2023-10-16 12:53:03 +0300

    add homework 3-4 tasks realisation

---
+++ db/migrate/20231016083744_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+
+      t.string :name, null: false
+      t.text :description
+      t.string :status, null: false
+      t.datetime :deadline_at
+      t.references :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
