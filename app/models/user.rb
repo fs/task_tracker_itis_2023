bf2e20247fde0d61db0019ca47c1fe60d758cc11
LeylaKhb <leyla.khbi@gmail.com>
2023-10-17 01:12:51 +0300

    HW6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9._-]+@[a-zA-Z]+.[a-zA-Z]+\z/,
+                                                                message: "incorrect email" }
   validates :first_name, :last_name, presence: true
+  validates :password, length: { minimum: 6 }
 end
