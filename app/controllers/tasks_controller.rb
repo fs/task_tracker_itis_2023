5896cb1a972ec83e626fa7c06f6f613dacc9ad3e
root <tammam744@gmail.com>
2023-10-17 12:02:09 +0300

    Edited the database to match the one on the public repo
    Any changes in the controller, the views, or the model file were just to make the naming of the variables
    match the new naming existing in the database!
    No changes for functions, nor new methods or views has been added!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,7 +59,7 @@ class TasksController < ApplicationController
 
   def create
     @task = @project.tasks.build(task_params)
-    @task.state = "Just Started"
+    @task.status = "Just Started"
 
     if deadline_correct?(@task)
       if @task.save
@@ -99,7 +99,7 @@ class TasksController < ApplicationController
   private
 
   def deadline_correct?(task)
-    !task.deadline.nil? and task.deadline >= Date.today
+    !task.deadline_at.nil? and task.deadline_at >= Date.today
   end
 
   def set_task
@@ -113,7 +113,7 @@ class TasksController < ApplicationController
   # end
 
   def task_params
-    params.require(:task).permit(:task_title, :task_description, :state, :deadline).merge(project_id: params[:project_id])
+    params.require(:task).permit(:name, :description, :status, :deadline_at).merge(project_id: params[:project_id])
 
   end
   def set_project
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_23_135510) do
+ActiveRecord::Schema.define(version: 2023_10_17_085257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
