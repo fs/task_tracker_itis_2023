1199799fbcd37eed8eda0a0db361dd177f1985d3
Denis Zakharov <78980031+DenisZackharov@users.noreply.github.com>
2023-10-17 15:56:37 +0300

    Revert "added homework (#36)" (#55)
    This reverts commit 872b576e3379db419bb65d0dc7552744bdecd9a1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
-  validates :password, length: { minimum: 6 }
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
 end
