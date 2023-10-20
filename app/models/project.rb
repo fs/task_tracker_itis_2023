5ca45d759281b0281dec1735214931c2aee783f9
STAFFHINO <yanro1410@mail.ru>
2023-10-20 13:09:13 +0300

    HW5

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
-  validates :name, :description, presence: true
+  validates :name, uniqueness: true, presence: true
+  validates :description, presence: true
   has_many :tasks, dependent: :destroy
 end
