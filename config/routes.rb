1612372be5f41ac59ff270beb212d2cfa21aa383
STAFFHINO <yanro1410@mail.ru>
2023-10-24 16:21:39 +0300

    Hw6 Sava

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :tasks
   end
 
+  delete "/logout", to: "sessions#destroy", as: "logout"
   resources :users, only: %i[new create]
-  resource :sessions, only: %i[new create show]
+  resource :sessions, only: %i[new create show destroy]
 end
