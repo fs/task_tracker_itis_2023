373d7f9ae95aa91b52ea7384008298fa4dd2e5d2
Ilya <mix.il2812@gmail.com>
2023-10-24 19:16:17 +0300

    dz6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
   validates :first_name, :last_name, presence: true
 end
