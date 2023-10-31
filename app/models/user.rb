f0bbee3ff71c0b4adb53001418dc9ae46d091ea3
sbgtlln <hamster132004@gmail.com>
2023-10-31 16:20:17 +0300

    added hw6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
+  validates :password, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root "projects#index"
+  get "login", to: "sessions#new", as: :login
+  post "login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
+
+  get "register", to: "users#new", as: :register
+  post "register", to: "users#create"
+
 
   resources :projects do
     resources :tasks
