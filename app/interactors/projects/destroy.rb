1f7e0085451f63486b294386b418640e2db52006
deniszackharov <denis.zaharov@flatstack.com>
2023-10-31 18:58:20 +0300

    add practice

--- app/interactors/projects/destroy.rb
+++ app/interactors/projects/destroy.rb
@@ -4,8 +4,18 @@ module Projects
 
     delegate :project, to: :context
 
+    before do
+      context.users = project.users
+    end
+
     def call
       project.destroy
+    end
+
+    after do
+      context.users.each do |user|
+        ProjectMailer.project_destroyed(user).deliver_later
+      end
     end
   end
 end
---
+++ app/interactors/tasks/create_default.rb
@@ -0,0 +1,30 @@
+module Tasks
+  class CreateDefault
+    include Interactor
+
+    delegate :project_id, to: :context
+
+    def call
+      context.fail!(error: "Invalid data") unless Task.create!(task_params)
+    end
+
+    private
+
+    def task_params
+      [
+        {
+          project_id: project_id,
+          name: "АНЖУМАНИЯ",
+          status: :unstarted,
+          deadline_at: 1.week.from_now
+        },
+        {
+          project_id: project_id,
+          name: "БЕГИТ",
+          status: :unstarted,
+          deadline_at: 1.week.from_now
+        }
+      ]
+    end
+  end
+end
