6b46f8db8cf559336f19a462c01eb8c701e94bdf
DotPointt <daukaevk@gmail.com>
2023-10-31 17:37:32 +0300

    added tasks (model/tabel? idk)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
+  has_many :tasks, dependent: :destroy
   validates :name, :description, presence: true
 end
---
+++ app/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ApplicationRecord
+    belongs_to :project
+    validates :name, presence: true
+end
