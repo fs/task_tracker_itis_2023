32d2409666e21154a372a2f37f170d804cd06338
polukarp <perzhul.works@gmail.com>
2023-11-02 21:25:31 +0300

    feat: 8th homework

---
+++ app/interactors/projects/update.rb
@@ -0,0 +1,14 @@
+module Projects
+  class Update
+    include Interactor
+
+    delegate :project, :project_params, to: :context
+
+    def call
+      context.project = project
+
+      context.fail!(error: "Invalid data") unless project.update(project_params)
+    end
+
+  end
+end
---
+++ app/interactors/tasks/create/prepare_params.rb
@@ -0,0 +1,19 @@
+module Tasks
+  class Create
+    class PrepareParams
+      include Interactor
+
+      delegate :params, to: :context
+
+      def call
+        context.task_params = prepared_task_params
+      end
+
+      private
+
+      def prepared_task_params
+        @prepared_task_params ||= params.require(:task).permit(:name, :description, :status, :deadline_at)
+      end
+    end
+  end
+end
