1e9cba1d39b6228021a64f910c77cce19ad301de
Denis Zakharov <78980031+DenisZackharov@users.noreply.github.com>
2023-11-03 11:36:30 +0300

    Fix interactor module names (#83)
    * fix interactor module names
    
    * fix mailer

--- app/interactors/projects/create.rb
+++ app/interactors/projects/create.rb
@@ -5,7 +5,7 @@ module Projects
     delegate :project, :user, to: :context
 
     organize Projects::Save,
-             Projects::Create::CreateOwner
+             Projects::Creates::CreateOwner
 
     after do
       ProjectMailer.project_created(project, user).deliver_later
