0e162d2ef36a032df356106dd2dd04f6fcec0da5
Denis Zakharov <78980031+DenisZackharov@users.noreply.github.com>
2023-11-03 11:52:55 +0300

    Add practice for lesson 9 (#92)
    * add practice
    
    * remove bang

--- app/interactors/projects/update.rb
+++ app/interactors/projects/update.rb
@@ -7,5 +7,11 @@ module Projects
     def call
       context.fail!(error: "Invalid data") unless project.update(project_params)
     end
+
+    after do
+      project.users.each do |user|
+        ProjectMailer.project_updated(project, user).deliver_later
+      end
+    end
   end
 end
