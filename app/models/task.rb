2559e95c6bcfdac0851bcc246550f7cfe52a2987
STAFFHINO <yanro1410@mail.ru>
2023-11-03 12:28:12 +0300

    Hw7-Sava

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,12 @@
 class Task < ApplicationRecord
+  extend Enumerize
   belongs_to :project
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :project_id }
-  validates :status, inclusion: { in: %w[unstarted started done] }
+  enumerize :status, in: %i[not_started started finished], default: :not_started
   validate :deadline_correct
+  has_many :comments
 
   private
 
---
+++ app/policy/user_policy.rb
@@ -0,0 +1,10 @@
+class UserPolicy < ApplicationPolicy
+    authorize :user, allow_nil: true
+    def new?
+      user.blank?
+    end
+  
+    def create?
+      user.blank?
+    end
+  end
