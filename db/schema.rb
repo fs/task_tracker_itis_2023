c6c7e27f496871a0c6bae80d79d1d0327697f29b
STAFFHINO <yanro1410@mail.ru>
2023-11-05 13:46:10 +0300

    Hw3-Sava_v1.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_19_141119) do
+ActiveRecord::Schema.define(version: 2023_11_05_104247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
