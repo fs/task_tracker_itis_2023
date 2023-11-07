941343c0918a697197d2b6f1eaa29e22657a00dd
Timur <timur.minullin535@gmail.com>
2023-11-07 16:09:24 +0300

    Add full HW9

--- app/interactors/tasks/update.rb
+++ app/interactors/tasks/update.rb
@@ -1,7 +1,13 @@
 module Tasks
   class Update
     include Interactor::Organizer
+    
+    delegate :task, :project, to: :context
 
     organize Tasks::Save
+
+    after do
+      TaskMailer.task_updated(project, task).deliver_later
+    end
   end
 end
