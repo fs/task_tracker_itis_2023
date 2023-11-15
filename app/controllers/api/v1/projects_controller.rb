a987bebe8aa3e233520c67a0319cbde761e312de
Timchik-IT <timur.minullin535@gmail.com>
2023-11-15 14:38:17 +0300

    add class work

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class ProjectsController < Api::ApplicationController
+      before_action :set_project, only: %i[ update destroy ]
       def index
         @projects = Project.includes(:tasks)
                            .order(params[:sort])
@@ -22,7 +23,39 @@ module Api
         end
       end
 
+      def update
+        @update_project = update_project
+        if @update_project.success?
+          render json: { project: @project, message: "Project updated" }
+        else
+          render json: { project: {}, errors: @project.errors }
+        end
+      end
+
+      def destroy
+        @destroy_project = destroy_project
+        if @destroy_project.success?
+          render json: { project: @project, message: "Project updated" }
+        else
+          render json: { project: {}, errors: @project.errors }
+        end
+      end
+
       private
+
+      def set_project
+        @project = Project.find_by(id: params[:id])
+      end
+
+      def destroy_project
+        @destroy_project ||= 
+          Projects::Destroy.call(project: @project)
+      end
+
+      def update_project
+        @update_project ||= 
+          Projects::Update.call(project: @project, project_params: project_params)
+      end
 
       def create_project
         @create_project ||=
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :projects, only: %i[index create]
+      resources :projects, only: %i[index create update destroy]
     end
   end
 
