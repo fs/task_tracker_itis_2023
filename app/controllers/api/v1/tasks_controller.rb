427cd824e334a35e2e386b8f962cd25c18bb8fe1
Timur <timur.minullin535@gmail.com>
2023-11-16 13:36:15 +0300

    robocop test

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TasksController < Api::ApplicationController
       before_action :set_project
-      before_action :set_task, only: %i[show edit update destroy]
+      before_action :set_task, only: %i[update destroy]
 
       def index
         @tasks = Task.includes(:comments)
