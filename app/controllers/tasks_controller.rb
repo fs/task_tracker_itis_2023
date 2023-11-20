4b508832f71eea729c508cd224b626975d1f9b14
DotPointt <daukaevk@gmail.com>
2023-11-20 17:53:25 +0300

    some minor fixes Andrey suggested

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_project
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, :set_task, only: %i[show edit update destroy]
 
   def index
     @tasks = @project.tasks
