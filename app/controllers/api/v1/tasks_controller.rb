ba186c3cd1eb7de193fdd4e60605bbd8e3d830a2
elenagernichenko <elena.gernichenko@yandex.ru>
2023-11-25 02:25:16 +0300

    added lesson practice & homework

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,68 @@
+module Api
+  module V1
+    class TasksController < Api::ApplicationController
+      before_action :set_project
+      before_action :set_task, only: %i[update destroy]
+      def index
+        @tasks = Task.order(params[:sort])
+                     .page(params[:page])
+                     .per(3)
+
+        serializable_tasks = ActiveModelSerializers::SerializableResource.new(
+          @tasks, each_serializer: TaskSerializer
+        )
+        render json: { tasks: serializable_tasks }
+      end
+
+      def create
+        @task = create_task.task
+        if create_task.success?
+          render json: { task: @task, message: "Task Created" }
+        else
+          render json: { task: {}, errors: @task.errors }
+        end
+      end
+
+      def update
+        @task = update_task.task
+        if update_task.success?
+          render json: { task: @task, message: "Task Updated" }
+        else
+          render json: { task: {}, errors: @task.errors }
+        end
+      end
+
+      def destroy
+        return unless destroy_task.success?
+
+        render json: { task: {}, errors: @task.id, message: "Task Destroyed" }
+      end
+
+      private
+
+      def create_task
+        @create_task ||= ::Tasks::Create.call(task_params: task_params, project: @project)
+      end
+
+      def update_task
+        ::Tasks::Update.call(task_params: task_params, task: @task)
+      end
+
+      def destroy_task
+        ::Tasks::Destroy.call(task: @task, user: current_user)
+      end
+
+      def task_params
+        params.require(:task).permit(:name, :description, :status, :deadline_at)
+      end
+
+      def set_project
+        @project = Project.find_by(id: params[:project_id])
+      end
+
+      def set_task
+        @task = @project.tasks.find(params[:id])
+      end
+    end
+  end
+end
