f3e14db689e302af0215aa17f8f652e4d487267d
deniszackharov <denis.zaharov@flatstack.com>
2023-11-27 23:23:17 +0300

    add enum status

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -7,7 +7,7 @@ module Types
     field :project_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :status, String, null: false
+    field :status, TaskStatusType, null: false
     field :project, ProjectType, null: false
   end
 end
