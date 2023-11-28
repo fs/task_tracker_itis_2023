997dbe2b9cf35e463fdb738c2c1992db71ed2e7d
Timur <timur.minullin535@gmail.com>
2023-11-28 17:31:55 +0300

    robocop

--- app/graphql/mutations/update_project.rb
+++ app/graphql/mutations/update_project.rb
@@ -5,7 +5,8 @@ module Mutations
     type Types::Payloads::CreateProjectPayload
 
     def resolve(input:)
-      result = Projects::Update.call(project: ::Project.find_by(id: input.id), project_params: input.to_h, user: current_user)
+      result = Projects::Update.call(project: ::Project.find_by(id: input.id), project_params: input.to_h,
+                                     user: current_user)
 
       result.to_h.merge(errors: formatted_errors(result.project))
     end
