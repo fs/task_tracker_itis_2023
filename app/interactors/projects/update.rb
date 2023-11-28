46c9ee12e8eb0e81822b868ab22ec164cf45d06a
LeylaKhb <leyla.khbi@gmail.com>
2023-11-28 17:32:59 +0300

    added updateProject, tasks and tasksByProjectId

--- app/interactors/projects/update.rb
+++ app/interactors/projects/update.rb
@@ -8,10 +8,10 @@ module Projects
       context.fail!(error: "Invalid data") unless project.update(project_params)
     end
 
-    # after do
-    #   project.users.each do |user|
-    #     ProjectMailer.project_updated(project, user).deliver_later
-    #   end
-    # end
+    after do
+      project.users.each do |user|
+        ProjectMailer.project_updated(project, user).deliver_later
+      end
+    end
   end
 end
