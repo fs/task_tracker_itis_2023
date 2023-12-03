890d91a85423b462b23c3121b5666ed1d8a73817
elenagernichenko <elena.gernichenko@yandex.ru>
2023-12-04 01:03:09 +0300

    added UpdateUser mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
     field :update_project, mutation: Mutations::UpdateProject
+    
+    field :update_user, mutation: Mutations::UpdateUser
   end
 end
