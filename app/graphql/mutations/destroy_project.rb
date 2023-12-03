57b7e15ecc2d3ef73c7770caee78093022deaf73
root <tammam744@gmail.com>
2023-12-03 16:33:17 +0300

    Mutaions, project destroy added

---
+++ app/graphql/mutations/destroy_project.rb
@@ -0,0 +1,15 @@
+module Mutations
+  class DestroyProject < BaseMutation
+    argument :input, Types::Inputs::DestroyProjectInput, required: true
+
+    type Types::Payloads::ProjectPayload
+
+    def resolve(input:)
+      @project = Project.find(input.id)
+
+      result = ::Projects::Destroy.call(project: @project)
+
+      result.to_h.merge(errors: formatted_errors(result.project))
+    end
+  end
+end
