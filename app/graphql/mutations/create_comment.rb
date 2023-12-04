9e392fd45f3f66dca3b204918b4ba449292de830
Leyla Khabibullina <113761333+LeylaKhb@users.noreply.github.com>
2023-12-04 22:23:19 +0300

    Update app/graphql/mutations/create_comment.rb
    Co-authored-by: Denis Zakharov <78980031+DenisZackharov@users.noreply.github.com>

--- app/graphql/mutations/create_comment.rb
+++ app/graphql/mutations/create_comment.rb
@@ -5,7 +5,9 @@ module Mutations
     type Types::Payloads::CommentPayload
 
     def resolve(input:)
-      result = Comments::Create.call(comment_params: input.to_h, task: Task.find_by(id: input.to_h[:task_id]), user: current_user)
+      hash_input = input.to_h
+      task = Task.find_by(id: hash_input.delete(:task_id))
+      result = Comments::Create.call(comment_params: hash_input, task: task, user: current_user)
 
       result.to_h.merge(errors: formatted_errors(result.comment))
     end
