ce42d398709f0397a0625f1dd0fbc7895e53394d
Timur <timur.minullin535@gmail.com>
2023-12-05 22:42:31 +0300

    extra point - destroy projects

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_project, mutation: Mutations::CreateProject
     field :update_project, mutation: Mutations::UpdateProject
     field :update_task, mutation: Mutations::UpdateTask
+    field :destroy_project, mutation: Mutations::DestroyProject
   end
 end
