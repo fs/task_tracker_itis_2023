4e5caf99f959f883f0e88f717be328b3048653e3
Atlasi1 <myltykbaev.artur@mail.ru>
2023-12-05 12:58:56 +0300

    added hw9

--- app/interactors/tasks/destroy.rb
+++ app/interactors/tasks/destroy.rb
@@ -2,10 +2,15 @@ module Tasks
   class Destroy
     include Interactor
 
-    delegate :task, to: :context
+    delegate :project, :task, to: :context
 
     def call
+      @task_name = task.name
       task.destroy
+    end
+
+    after do
+      TaskMailer.task_destroy(project, @task_name).deliver_later
     end
   end
 end
