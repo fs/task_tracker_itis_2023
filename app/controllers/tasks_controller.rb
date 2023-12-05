811b93563040f38100cd94c8a767fa4f8bd2b872
kamilla111 <kamilla.sirazova@bk.ru>
2023-12-05 16:51:22 +0300

    hw3-4

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  before_action :set_project
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = @project.tasks
+  end
+
+  def show
+  end
+
+  def new
+    @task = @project.tasks.build
+    @task.deadline_at ||= 1.week.from_now
+  end
+
+  def create
+    @task = @project.tasks.build(task_params)
+
+    if @task.save
+      redirect_to project_tasks_path(@project), notice: "Task created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to project_tasks_path(@project), notice: "Task updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to project_tasks_path(@project), notice: "Task destroyed"
+  end
+
+  private
+
+  def set_project
+    @project = Project.find_by(id: params[:project_id])
+  end
+
+  def set_task
+    @task = @project.tasks.find_by!(id: params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :status, :deadline_at)
+  end
+end
