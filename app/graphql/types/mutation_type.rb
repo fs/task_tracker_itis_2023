a65519947300bcb9d3fb62253d50b4085828888f
Timur <timur.minullin535@gmail.com>
2023-12-05 16:20:10 +0300

    add HW12 - Adela

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
     field :update_project, mutation: Mutations::UpdateProject
-    field :update_task, mutation: Mutations::UpdateTask
+    field :update_task, mutation: Mutations::UpdateTask 
+    field :destroy_project, mutation: Mutations::DestroyProject
   end
 end
