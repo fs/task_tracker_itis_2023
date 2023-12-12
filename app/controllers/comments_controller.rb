0dc01e150ae1aa9deb25544c6c9a6419587bcf5d
svnndn <svnndndn@gmail.com>
2023-12-12 17:27:04 +0300

    add hw7

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,54 @@
+class CommentsController < ApplicationController
+  before_action :set_task
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
+  def edit
+    authorize! @comment
+  end
+
+  def create
+    @comment = @task.comments.new(comment_params)
+    @comment.user = current_user
+
+    authorize! @comment
+    if @comment.save
+      redirect_to project_task_path(@task.project, @task), notice: "Comment created successfully"
+    else
+      redirect_to project_task_path(@task.project, @task), alert: "Failed to create comment"
+    end
+  end
+
+  def update
+    authorize! @comment
+
+    if @comment.update(comment_params)
+      redirect_to project_task_path(@task.project, @task), notice: "Comment updated successfully"
+    else
+      redirect_to project_task_path(@task.project, @task), alert: "Failed to update comment"
+    end
+  end
+
+  def destroy
+    authorize! @comment
+
+    if @comment.destroy
+      redirect_to project_task_path(@task.project, @task), notice: "Comment deleted successfully"
+    else
+      redirect_to project_task_path(@task.project, @task), alert: "Failed to delete comment"
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,11 @@ class TasksController < ApplicationController
   before_action :set_project
   before_action :set_task, only: %i[show edit update destroy]
 
+  before_action -> { authorize! @task }, only: %i[update show destroy]
+
   def index
+    @task = Task.new(project: @project)
+    authorize! @task
     @tasks = @project.tasks.order(params[:sort]).page(params[:page]).per(3)
   end
 
@@ -11,12 +15,14 @@ class TasksController < ApplicationController
   def new
     @task = @project.tasks.build
     @task.deadline_at ||= 1.week.from_now
+    authorize! @task
   end
 
   def edit; end
 
   def create
     @task = @project.tasks.build(task_params)
+    authorize! @task
 
     if @task.save
       redirect_to project_tasks_path(@project), notice: "Task created successfully"
