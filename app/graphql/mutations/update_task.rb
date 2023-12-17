04444739214b9558955399a2a3738a620d413a99
deniszackharov <denis.zaharov@flatstack.com>
2023-12-18 01:49:24 +0300

    little fix

--- app/graphql/mutations/update_task.rb
+++ app/graphql/mutations/update_task.rb
@@ -5,9 +5,11 @@ module Mutations
     type Types::Payloads::TaskPayload
 
     def resolve(input:)
+      input_hash = input.to_h
+
       result = ::Tasks::Update.call(
-        task: Task.find_by(id: input.to_h.delete(:id)),
-        task_params: input.to_h, user: current_user
+        task: Task.find_by(id: input_hash.delete(:id)),
+        task_params: input_hash, user: current_user
       )
 
       result.to_h.merge(errors: formatted_errors(result.task))
--- app/graphql/types/payloads/task_payload.rb
+++ app/graphql/types/payloads/task_payload.rb
@@ -2,7 +2,7 @@ module Types
   module Payloads
     class TaskPayload < Types::BaseObject
       field :task, TaskType, null: true
-      field :errors, [Types::UserError], null: true
+      field :errors, [Types::UserError], null: false
     end
   end
 end
