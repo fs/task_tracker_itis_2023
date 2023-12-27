5d0e8f807af770918a470b7f476d32db1a2a0522
nikita <evstyagin09@gmail.com>
2023-12-27 17:35:03 +0300

    ready HW11

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class ProjectsController < Api::ApplicationController
+      before_action :set_project, only: %i[update destroy]
       def index
         @projects = Project.includes(:tasks)
                            .order(params[:sort])
@@ -16,10 +17,24 @@ module Api
       def create
         @project = create_project.project
         if create_project.success?
-          render json: { project: @project, message: "Project Created" }
+          render json: { project: @project, message: "Project created" }
         else
           render json: { project: {}, errors: @project.errors }
         end
+      end
+
+      def update
+        if update_project.success?
+          render json: { project: @project, message: "Project update successful" }
+        else
+          render json: { message: "Error" }
+        end
+      end
+
+      def destroy
+        return unless destroy_project.success?
+
+        render json: { message: "Project destroyed" }
       end
 
       private
@@ -29,6 +44,19 @@ module Api
           Projects::Create.call(project_params: project_params, user: current_user)
       end
 
+      def update_project
+        ::Projects::Update.call(project: @project,
+                                project_params: project_params)
+      end
+
+      def destroy_project
+        ::Projects::Destroy.call(project: @project)
+      end
+
+      def set_project
+        @project = Project.find_by(id: params[:id])
+      end
+      
       def project_params
         params.require(:project).permit(:name, :description)
       end
