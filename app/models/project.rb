e374716ac730ea29973b2c1f5ac1c74a07b83b04
Цветолина Евсеева <cvetolinaevseeva@MacBook-Pro-Cvetolina.local>
2024-01-03 01:32:30 +0300

    add hw 3-4

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   validates :name, :description, presence: true
+
+  has_many :tasks, dependent: :destroy
+
 end
---
+++ app/models/task.rb
@@ -0,0 +1,15 @@
+class Task < ApplicationRecord
+
+    belongs_to :project
+  
+    validates :name, presence: true
+    validates :status, inclusion: { in: ["unstarted", "started", "done"]}
+    validate :deadline_correct
+  
+    private
+  
+    def deadline_correct
+      return if (created_at || Time.current) < deadline_at
+      errors.add(:deadline_at, "must be after creation time")
+    end
+  end
