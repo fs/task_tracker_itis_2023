aabe0335055a749dd0ee4e98a204ea7fb84056c7
Цветолина Евсеева <cvetolinaevseeva@MacBook-Pro-Cvetolina.local>
2024-01-03 01:13:13 +0300

    add hw6

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :tasks
   end
 
-  resources :users, only: %i[new create]
-  resource :sessions, only: %i[new create show]
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
