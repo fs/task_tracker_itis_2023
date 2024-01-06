7ce7f819e9a87ffa06ef349c87e37d8704085222
STAFFHINO <yanro1410@mail.ru>
2024-01-06 15:00:38 +0300

    Hw11-Sava

---
+++ app/serializers/api/v1/task_serializer.rb
@@ -0,0 +1,14 @@
+module Api
+    module V1
+      class TaskSerializer < ActiveModel::Serializer
+        attributes :id, :name, :description, :status
+        attribute :deadline_at do
+          object.deadline_at.strftime("%d-%m-%Y")
+        end
+  
+        has_many :comments
+        belongs_to :project
+      end
+    end
+  end
+  
