f5e5a8024def8bae8a1fdbe3c9ecd6233d4b0f30
DotPointt <daukaevk@gmail.com>
2024-01-08 23:56:15 +0300

    in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-  validates :first_name, :last_name, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :tasks
   end
 
+  get "/login", to: "sessions#new", as: "login"
+  get "/register", to: "users#new", as: "register"
+
   resources :users, only: %i[new create]
   resource :sessions, only: %i[new create show]
+
 end
